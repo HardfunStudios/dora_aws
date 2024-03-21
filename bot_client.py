ca9feefbce4fc64b527280c903cc6a0cdc9ece2a
José Pedro Schardosim Simão <jose@hardfun.com.br>
2024-03-21 14:03:04 -0300

    Try not to timeout

--- bot_client.py
+++ bot_client.py
@@ -1,13 +1,21 @@
 import boto3
+import botocore
 import os
 
 class BotClient:
     def __init__(self):
+        config = botocore.config.Config(
+            read_timeout=900,
+            connect_timeout=900,
+            retries={"max_attempts": 0},
+            tcp_keepalive=True
+        )
         self.client = boto3.client(
             service_name='bedrock-agent-runtime',
             region_name='us-east-1',
             aws_access_key_id=os.environ['ACCESS_KEY'],
-            aws_secret_access_key=os.environ['SECRET_KEY']
+            aws_secret_access_key=os.environ['SECRET_KEY'],
+            config=config
         )
 
     def send_message(self, prompt, session_id, session_attributes, prompt_attributes, agent_settings):
