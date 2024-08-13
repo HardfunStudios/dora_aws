47ae6258b65d0bb4143f77802485b9e4e35d4018
José Pedro Schardosim Simão <jose@hardfun.com.br>
2024-08-13 17:04:29 -0300

    Test

--- app.py
+++ app.py
@@ -44,9 +44,9 @@ def sync_content():
     course_content = request.json['course_content']
     data = request.json['data']
     metadata = request.json['metadata']
-    agent_data = json.loads(request.json['agent_data'])
+    agent_data = request.json['agent_data']
+    agent_id = agent_data['agent_id']
 
-    print(agent_data)
     try:
         sync_client = SyncClient(
             boto3_session=boto3_session,
@@ -69,7 +69,7 @@ def sync_content():
             agent_data=agent_data
         )
         response = sync_client.create_course_knowledge_base(course_id=course_id, course_content=course_content, metadata=metadata, data=data)
-        bot_client._prepare_agent(agent_id=agent_id)
+        bot_client._prepare_agent(agent_id=agent_id[])
         return response, 200
     except Exception as e:
         return str(e), 500
